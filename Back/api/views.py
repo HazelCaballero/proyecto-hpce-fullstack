458829e902241f5c60da894d08d84aae82499efb
HazelCaballero <hazelcabaelleroelizondo@gmail.com>
2025-05-28 09:46:03 -0600

    28/05/2025 pruebas de autenticacion

--- Back/api/views.py
+++ Back/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import (
     CustomUser, Categoria, Trueque, Publicacion, InteraccionPublicacion,
     Servicio, InteraccionTrueque, Publicidades, Contactos
@@ -18,99 +18,121 @@ class IsSuperUser(BasePermission):
   
     def has_permission(self, request, view):
         return request.user and request.user.is_superuser
+    
 
-class CustomUserListCreateView(generics.ListCreateAPIView):
+class CustomUserListCreateView(ListCreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    
+
+class CustomUserDetailView(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
 
-class CustomUserDetailView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
-    queryset = CustomUser.objects.all()
-    serializer_class = CustomUserSerializer
 
-class CategoriaListCreateView(generics.ListCreateAPIView):
+class CategoriaListCreateView(ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
 
-class CategoriaDetailView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
+class CategoriaDetailView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated, IsSuperUser]
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
 
-class TruequeListCreateView(generics.ListCreateAPIView):
+
+
+class TruequeListCreateView(ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Trueque.objects.all()
     serializer_class = TruequeSerializer
 
-class TruequeDetailView(generics.RetrieveUpdateDestroyAPIView):
+class TruequeDetailView(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Trueque.objects.all()
     serializer_class = TruequeSerializer
 
-class PublicacionListCreateView(generics.ListCreateAPIView):
+
+
+class PublicacionListCreateView(ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Publicacion.objects.all()
     serializer_class = PublicacionSerializer
 
-class PublicacionDetailView(generics.RetrieveUpdateDestroyAPIView):
+class PublicacionDetailView(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Publicacion.objects.all()
     serializer_class = PublicacionSerializer
 
-class InteraccionPublicacionListCreateView(generics.ListCreateAPIView):
+
+
+class InteraccionPublicacionListCreateView(ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     queryset = InteraccionPublicacion.objects.all()
     serializer_class = InteraccionPublicacionSerializer
 
-class InteraccionPublicacionDetailView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
+class InteraccionPublicacionDetailView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated, IsSuperUser]
     queryset = InteraccionPublicacion.objects.all()
     serializer_class = InteraccionPublicacionSerializer
+    
+    
+    
 
-class ServicioListCreateView(generics.ListCreateAPIView):
+class ServicioListCreateView(ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Servicio.objects.all()
     serializer_class = ServicioSerializer
 
-class ServicioDetailView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
+class ServicioDetailView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated, IsSuperUser]
     queryset = Servicio.objects.all()
     serializer_class = ServicioSerializer
 
-class InteraccionTruequeListCreateView(generics.ListCreateAPIView):
+
+
+class InteraccionTruequeListCreateView(ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     queryset = InteraccionTrueque.objects.all()
     serializer_class = InteraccionTruequeSerializer
 
-class InteraccionTruequeDetailView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
+class InteraccionTruequeDetailView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated, IsSuperUser]
     queryset = InteraccionTrueque.objects.all()
     serializer_class = InteraccionTruequeSerializer
+    
+    
+    
 
-class PublicidadesListCreateView(generics.ListCreateAPIView):
+class PublicidadesListCreateView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Publicidades.objects.all()
+    serializer_class = PublicidadesSerializer
+
+class PublicidadesDetailView(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated, IsSuperUser]
     queryset = Publicidades.objects.all()
     serializer_class = PublicidadesSerializer
 
-class PublicidadesDetailView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated, IsSuperUser]
-    queryset = Publicidades.objects.all()
-    serializer_class = PublicidadesSerializer
 
-class ContactosListCreateView(generics.ListCreateAPIView):
+
+
+class ContactosListCreateView(ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Contactos.objects.all()
     serializer_class = ContactosSerializer
 
-class ContactosDetailView(generics.RetrieveUpdateDestroyAPIView):
+class ContactosDetailView(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated, IsSuperUser]
     queryset = Contactos.objects.all()
     serializer_class = ContactosSerializer
+    
+    
+    
 
 class CrearSuperUsuario(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsSuperUser]
     def post(self,request):
         usuername = request.data.get('username')
         password = request.data.get('password')
@@ -122,15 +144,19 @@ class CrearSuperUsuario(APIView):
         
         return Response({"message": "Superusuario creado exitosamente."}, status=status.HTTP_201_CREATED)
     
+    
+    
 class VerSuperUsuario(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsSuperUser]
     def get(self, request):
         superusers = User.objects.filter(is_superuser=True)
         serializer = CustomUserSerializer(superusers, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
+    
+    
 class EliminarSuperUsuario(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsSuperUser]
     def delete(self, request, pk):
         try:
             superuser = User.objects.get(pk=pk, is_superuser=True)
@@ -139,8 +165,10 @@ class EliminarSuperUsuario(APIView):
         except User.DoesNotExist:
             return Response({"error": "Superusuario no encontrado."}, status=status.HTTP_404_NOT_FOUND)
 
+
+
 class ActualizarSuperUsuario(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsSuperUser]
     def put(self, request, pk):
         try:
             superuser = User.objects.get(pk=pk, is_superuser=True)
